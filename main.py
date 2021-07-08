b8fcc912d856ec17eb40ae3613c3461888cd70a3
AkshithKachaka1 <67799241+Akshith-coder@users.noreply.github.com>
2021-07-08 17:00:37 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#Integer
+''''
+Integers are numbers in Python
+'''
+#int() function
+''''
+You can use the int() function to turn any specific value to an integer number
+'''
+x = "25"
+print(int(x))
+print(type(x))
